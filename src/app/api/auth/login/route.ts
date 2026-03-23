1867ba26734bc721a483b3d9ead142a1ab180c38
Varun <vpahuja1508@gmail.com>
2026-03-23 15:23:35 +0530

    prod oauth

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,5 +1,6 @@
 const client_id = process.env.SPOTIFY_CLIENT_ID!;
-const redirect_uri = "http://127.0.0.1:3000/api/auth/callback";
+// const redirect_uri = "http://127.0.0.1:3000/api/auth/callback";
+const redirect_uri = "https://nowcard.vercel.app/api/auth/callback";
 
 export async function GET() {
   const scope = "user-read-currently-playing user-read-recently-played";
