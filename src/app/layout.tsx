b2e9cbbcf3a30c466789315ee57de788a2723443
Varun <vpahuja1508@gmail.com>
2026-03-25 09:42:54 +0530

    Bug Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      domain="nowcard.store"
+      dynamic
+    >
       <html
         lang="en"
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
